2fcc059fac4b6ad17d2c10ff16a5e76a5d48e0c7
Ben Wilhelm <ben.wilhelm@fullstackacademy.com>
2018-10-29 10:19:00 -0500

    adds post route for /carts/

---
+++ server/api/cart.js
@@ -0,0 +1,17 @@
+const { Router } = require('express')
+const Cart = require('../db/cart')
+
+const router = Router()
+
+router.get('/', async (req, res, next) => {
+  try {
+    const carts = awaitCart.findAll()
+    return res.json(carts)
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.post('/', async(req,res, next)=>{
+  //...
+})
